a8309937a84d7c62f8e7a1ef0dc7b6b569e5152e
user_name <email_id>
2023-03-12 23:56:53 -0400

    bohan

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,13 +1,25 @@
 import requests
 import json
 # import related models here
+from .models import CarDealer
 from requests.auth import HTTPBasicAuth
 
 
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
-
+def get_request(url, **kwargs):
+    print(kwargs)
+    print('GET from {}'.format(url))
+    try:
+        response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                params=kwargs)
+    except:
+        print('Network exception occurred')
+    status_code = response.sataus_code
+    print('With status {} '.format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
